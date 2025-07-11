dcb326cbe56d66729dc47ea823f66a871cd07345
sharif2595 <zafarshareef3@gmail.com>
2025-07-11 21:04:21 +0530

    Update BaseClass.java

--- src/test/java/modulepackage/Pom_project/BaseClass.java
+++ src/test/java/modulepackage/Pom_project/BaseClass.java
@@ -19,6 +19,8 @@ public class BaseClass extends Itest_listener_logic {
 	@BeforeClass
 	public void launch_browser() throws InterruptedException
 	{
+		WebDriverManager.edgedriver().setup();
+		 driver = new EdgeDriver();
 		driver= new EdgeDriver();
 		driver.get(given_url); 
 		driver.manage().window().maximize();
